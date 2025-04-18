cfc7bd331fd52cd70afee8f29ead6f40ee6afd3a
Maekzehn <pakphum.mc@gmail.com>
2025-04-18 18:15:37 +0700

    fix

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-def main(name: str):
-    return {"message": "Hello, World!", "name": name}
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.post("/")
+def read_root(request):
+    return {"message": "Hello, World!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
