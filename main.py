8ae2ede49b74411942b5b77aa0854dda7dfe85fc
Maekzehn <pakphum.mc@gmail.com>
2025-04-18 18:45:30 +0700

    test

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-def start_cdc_trading(name: str):
-    return {"message": "Hello, World!", "name": str}
+def start_cdc_trading(request):
+    request_json = request.get_json()
+    name = request_json.args.get("name")
+    return {"message": "Hello, World!", "name": name}
