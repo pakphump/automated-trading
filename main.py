853271773398ba94cb3cd449ab5822b076618a51
Maekzehn <pakphum.mc@gmail.com>
2025-04-20 21:36:43 +0700

    exp

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from automated_trading import AutomatedTrading
 from automated_trading.models import CdcStrategyHandlerRequest
 from datetime import datetime
+import functions_framework
+from flask import Request
 
 automated_trading = AutomatedTrading()
 
 
-def cdc_strategy_handler(request):
-    request_json = request.json()
+@functions_framework.http
+def cdc_strategy_handler(request: Request):
+
+    request_json = request.get_json()
     print(request_json)
+
     request_model = CdcStrategyHandlerRequest(**request_json)
 
     result = automated_trading.run_cdc_strategy(
