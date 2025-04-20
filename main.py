b8bda89607a5a6e68e557f533e0d39154ce3d080
Maekzehn <pakphum.mc@gmail.com>
2025-04-20 21:24:58 +0700

    fix type

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from datetime import datetime
 automated_trading = AutomatedTrading()
 
 
-def cdc_strategy_handler(request: dict):
-    request_model = CdcStrategyHandlerRequest(**request)
+def cdc_strategy_handler(request):
+    request_model = CdcStrategyHandlerRequest(**request.json())
 
     result = automated_trading.run_cdc_strategy(
         symbol=request_model.symbol, interval=request_model.interval
