8dae3ed91ab9aaf7b8bd3a911eab30ed6e99720b
Maekzehn <pakphum.mc@gmail.com>
2025-04-21 20:39:12 +0700

    fix get current position mode

--- automated_trading/binance_apis/account.py
+++ automated_trading/binance_apis/account.py
@@ -1,10 +1,10 @@
 import requests
-from automated_trading.utils import create_signature
+from automated_trading.utils import add_signature_in_payload
 from automated_trading.binance_apis.market_data import get_server_time
 from automated_trading.models import *
 
 
-def get_current_position_mode(api_key: str, secret_ket: str):
+def get_current_position_mode(api_key: str, secret_key: str) -> bool:
 
     server_time = get_server_time()
 
@@ -14,11 +14,12 @@ def get_current_position_mode(api_key: str, secret_ket: str):
     )
 
     # Add signature
-    signature = create_signature(secret_ket, payload)
-    payload["signature"] = signature
-
+    payload = add_signature_in_payload(secret_key, payload)
     headers = {"X-MBX-APIKEY": api_key}
 
     response = requests.request(**url_n_method, params=payload, headers=headers)
+    response.raise_for_status()
 
-    return response.json()
+    response_model = GetCurrentPositionModeResponse(**response.json())
+
+    return response_model.dual_side_position
