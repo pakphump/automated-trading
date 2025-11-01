a83451352298acc2d94dd65533eadd5d439f0863
Maekzehn <pakphum.mc@gmail.com>
2025-11-01 23:05:36 +0700

    experiment

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from automated_trading import AutomatedTrading
-from automated_trading.exchanges.binance_apis import *
+# from automated_trading import AutomatedTrading
+# from automated_trading.exchanges.binance_apis import *
 import functions_framework
 from flask import Request
 
-automated_trading = AutomatedTrading()
+# automated_trading = AutomatedTrading()
 
 
-@functions_framework.http
-def cdc_strategy_handler(request: Request):
+# @functions_framework.http
+# def cdc_strategy_handler(request: Request):
 
-    request_json = request.get_json()
+#     request_json = request.get_json()
 
-    response = automated_trading.run_cdc_strategy(
-        symbol=request_json["symbol"],
-        interval=request_json["interval"],
-        quantity=request_json["quantity"],
-        api_key=request_json["api_key"],
-        secret_key=request_json["secret_key"],
-    )
+#     response = automated_trading.run_cdc_strategy(
+#         symbol=request_json["symbol"],
+#         interval=request_json["interval"],
+#         quantity=request_json["quantity"],
+#         api_key=request_json["api_key"],
+#         secret_key=request_json["secret_key"],
+#     )
 
-    return response
+# return response
 
 
 @functions_framework.http
