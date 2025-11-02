e1d39c608132dedf3cafd4feed3bb95d406837e3
Maekzehn <pakphum.mc@gmail.com>
2025-11-02 16:08:49 +0700

    add OrderApi

--- automated_trading/exchanges/kucoin_fapis/kucoin_future_api_manager.py
+++ automated_trading/exchanges/kucoin_fapis/kucoin_future_api_manager.py
@@ -1,6 +1,6 @@
-from .apis import AccountApi, MarketApi
+from .apis import AccountApi, MarketApi, OrderApi
 
 
-class KucoinFutureApiManager(AccountApi, MarketApi):
+class KucoinFutureApiManager(AccountApi, MarketApi, OrderApi):
     def __init__(self, api_key, api_secret, api_passphrase):
         super().__init__(api_key, api_secret, api_passphrase)
